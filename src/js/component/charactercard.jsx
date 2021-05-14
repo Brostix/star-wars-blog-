46b0306f6e93426af77bb432258b2c12c7786950
stefanobelfiore <stefanobelfiore@yahoo.com>
2021-05-14 11:03:04 +0000

    bien

--- src/js/component/charactercard.jsx
+++ src/js/component/charactercard.jsx
@@ -8,28 +8,29 @@ const CharacterCard = props => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<Card style={{ width: "18rem" }}>
+		<Card className="container my-3" style={{ width: "18rem" }}>
 			<Card.Img variant="top" src="https://images-na.ssl-images-amazon.com/images/I/51MD9V7hvsL.jpg" />
 			<Card.Body>
 				<Card.Title>{props.name}</Card.Title>
 				<Card.Text>
 					Some quick example text to build on the card title and make up the bulk of the cards content.
 				</Card.Text>
+				<div className="myButtons">
+					<Link to="/charactersdetails">
+						<button className="btn btn-dark">
+							<i className="informations fas fa-info-circle" />
+						</button>
+					</Link>
 
-				<Link to="/charactersdetails">
-					<button className="btn btn-dark">
-						<i className="informations fas fa-info-circle" />
-					</button>
-				</Link>
-
-				<Button
-					onClick={() => {
-						actions.setFavourite(props.name);
-					}}
-					className={"favourites"}
-					variant="warning">
-					😍
-				</Button>
+					<Button
+						onClick={() => {
+							actions.setFavourite(props.name);
+						}}
+						className={"favourites"}
+						variant="warning">
+						😍
+					</Button>
+				</div>
 			</Card.Body>
 		</Card>
 	);
