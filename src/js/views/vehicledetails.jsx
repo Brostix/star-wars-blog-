e7f927d484ab7259cfbba03b75b39d97e669eff9
Mario Sorrentino <eclest@gmail.com>
2021-05-15 14:38:46 +0000

    estilos del background y cards

--- src/js/views/vehicledetails.jsx
+++ src/js/views/vehicledetails.jsx
@@ -8,17 +8,19 @@ export const VehicleDetails = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="mycard card my-3 container" style={{ width: "25rem" }}>
+		<div className="myvehicles card">
 			<img
-				className="card-img-top my-3 container"
+				className="imgcss card-img-top my-3 container"
 				src="https://www.10wallpaper.com/wallpaper/1366x768/1512/Star_wars_fighter-PC_Game_HD_Wallpapers_1366x768.jpg"
 				alt=""
 			/>
 			<div className="card-body">
-				<h5 className="card-title">Description</h5>
-				<p className="card-text">
+				<h5 className="cardtextdetails card-title">Description</h5>
+
+				<p className="cardtextdetails card-text">
 					Some quick example text to build on the card title and make up the bulk of the cards content.
 				</p>
+
 				<Link to="/">
 					<button className="btn btn-dark offset">More Details</button>
 				</Link>
