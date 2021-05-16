b89c42a8c6903c53fc58644c341c04d76416f798
Mario Sorrentino <eclest@gmail.com>
2021-05-16 16:08:14 +0000

    test 1

--- src/js/views/vehicledetails.jsx
+++ src/js/views/vehicledetails.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-
 import "../../styles/vehiclesdetails.scss";
 
 export const VehicleDetails = () => {
